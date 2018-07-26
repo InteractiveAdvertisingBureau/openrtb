b76c3f08e552d7f9b1c90beb808812d7a0cff3d1
Zach Badgett <zach.badgett@gmail.com>
2018-07-26 11:14:48 -0600

    Update Scala example with suggested changes

--- ads-cert-examples/scala/src/main/scala/Main.scala
+++ ads-cert-examples/scala/src/main/scala/Main.scala
@@ -16,14 +16,11 @@ import scala.util.{Failure, Success, Try}
 
 object Main {
 
-  private var privateKey: PrivateKey = _
-  private var publicKey: PublicKey = _
-
   Security.addProvider(new BouncyCastleProvider())
 
   def main(args: Array[String]): Unit = {
-    loadKeys(args)
-    val message = Stream(
+
+    val message = Seq(
       "2824f5d3-a2dc-49ec-8358-dbe92bd01ec2", // source.tid
       "a:180720yz:1807241326", // ad.pcv
       "newsite.com", // site.domain
@@ -35,51 +32,56 @@ object Main {
       "", // device.ifa
       "", // device.ua
       "" // ad.video player size?
-    ).map(Option(_).getOrElse("")).toList.mkString(":")
-    for {
-      signature <- sign(Signature.getInstance("SHA256withECDSA"), message)
-    } yield {
-      printf("publisher signature: %s\n", Base64.encodeBase64String(signature))
-      Try {
-        verify(Signature.getInstance("SHA256withECDSA"), message, signature)
-      } match {
-        case Success(b) => b.getOrElse(false) match {
-          case true => println("yay it worked!")
-          case false => println("it didn't work :(")
-        }
-        case Failure(e) => throw e
-      }
+    ).mkString(":")
+
+    val tryResult = for {
+      (privateKey, publicKey) <- loadKeys(args)
+      signature <- sign(privateKey, Signature.getInstance("SHA256withECDSA"), message)
+      _ <- Try(println(s"publisher signature: ${Base64.encodeBase64String(signature)}"))
+      result <- verify(publicKey, Signature.getInstance("SHA256withECDSA"), message, signature)
+    } yield result
+
+    tryResult match {
+      case Success(true) => println("yay it worked!")
+      case Success(false) => println("it didn't work :(")
+      case Failure(e) => println(e)
     }
   }
 
-  private def loadKeys(args: Array[String]): Try[_] = {
+  private def loadKeys(args: Array[String]): Try[(PrivateKey, PublicKey)] = {
     if (args.length < 2) {
-      Failure(new IllegalArgumentException("invalid argument length"))
+      throw new IllegalArgumentException("invalid argument length")
     }
     val privateParser = new PEMParser(new InputStreamReader(new FileInputStream(args(0))))
-    val oid = privateParser.readObject.asInstanceOf[ASN1ObjectIdentifier]
-    val spec = ECNamedCurveTable.getByOID(oid)
-    if (spec == null) Failure(new RuntimeException("spec not found for named curve"))
-    val pemPair = privateParser.readObject.asInstanceOf[PEMKeyPair]
+    val oid = privateParser.readObject match {
+      case o: ASN1ObjectIdentifier => o
+      case _ => throw new ClassCastException
+    }
+    val spec = Option(ECNamedCurveTable.getByOID(oid))
+    if (spec.isEmpty) throw new RuntimeException("spec not found for named curve")
+    val pemPair = privateParser.readObject match {
+      case p: PEMKeyPair => p
+      case _ => throw new ClassCastException
+    }
     val pair = new JcaPEMKeyConverter().setProvider("BC").getKeyPair(pemPair)
-    privateKey = pair.getPrivate
+    val privateKey = pair.getPrivate
     // note: you can just use:
     // publicKey = pair.getPublic();
     // however, for this example I want to be able to specify an incorrect public key
     // load public key
     val publicParser = new PEMParser(new InputStreamReader(new FileInputStream(args(1))))
     val publicObj = publicParser.readPemObject
-    publicKey = KeyFactory.getInstance("EC", "BC").generatePublic(new X509EncodedKeySpec(publicObj.getContent))
-    Success(())
+    val publicKey = KeyFactory.getInstance("EC", "BC").generatePublic(new X509EncodedKeySpec(publicObj.getContent))
+    Success((privateKey, publicKey))
   }
 
-  private def sign(digest: Signature, message: String): Try[Array[Byte]] = {
+  private def sign(privateKey: PrivateKey, digest: Signature, message: String): Try[Array[Byte]] = {
     digest.initSign(privateKey)
     digest.update(message.getBytes(StandardCharsets.UTF_8))
     Success(digest.sign)
   }
 
-  private def verify(digest: Signature, message: String, signature: Array[Byte]): Try[Boolean] = {
+  private def verify(publicKey: PublicKey, digest: Signature, message: String, signature: Array[Byte]): Try[Boolean] = {
     digest.initVerify(publicKey)
     digest.update(message.getBytes(StandardCharsets.UTF_8))
     Success(digest.verify(signature))
